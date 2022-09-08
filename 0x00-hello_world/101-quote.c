5ece63b45f1964487f43c51c52e92bca1cc08adf
nzakawa <nzakawa1@gmail.com>
2022-09-08 11:48:57 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#iclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and test piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
