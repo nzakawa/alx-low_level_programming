3e89244891af0c13259623df515e7ed3cf9d94c9
nzakawa <nzakawa1@gmail.com>
2022-09-08 11:53:48 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and test piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and test piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
