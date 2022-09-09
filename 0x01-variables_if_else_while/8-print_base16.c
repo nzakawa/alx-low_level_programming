d992b262204db9f388ff3bdcabe2d97a6d67e5c8
nzakawa <nzakawa1@gmail.com>
2022-09-09 14:35:24 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main -  prints all possible combinations of single-digit numbers
  * Return: 0
  */
-
 int main(void)
 {
 int num;
@@ -13,12 +11,10 @@ for (num = 48; num <= 57; num++)
 {
 putchar(num);
 }
-
 for (letter = 'a'; letter <= 'f'; letter++)
 {
 putchar(letter);
 }
-
 putchar('\n');
 
 return (0);
