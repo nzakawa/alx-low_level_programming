7798ede86f04de6acec1744ae3aa728bf7be58a9
nzakawa <nzakawa1@gmail.com>
2022-09-09 12:54:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++);
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
