72ec819c8fe2c7fb89d28e4b13d34b919b335324
nzakawa <nzakawa1@gmail.com>
2022-09-09 13:54:44 -0700

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int mai(void)
+{
+	int num;
+	char letter;
+	
+	for (num 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
