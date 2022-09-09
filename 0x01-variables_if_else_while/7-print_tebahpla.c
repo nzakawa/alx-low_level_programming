a093f3e81fdd5169b21e143b2226662c3d6cf642
nzakawa <nzakawa1@gmail.com>
2022-09-09 13:29:48 -0700

    preint tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l_letter = 122;
+
+	while (l-letter >= 97)
+	{
+		putchar(l-letter);
+		l-letter--;
+	}
+
+	purchat('\n');
+
+	return (0);
+}
