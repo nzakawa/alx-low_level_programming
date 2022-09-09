6fe9899216fab00f23f31a924562ee9333922392
nzakawa <nzakawa1@gmail.com>
2022-09-09 11:27:16 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints lowercase in alphabet
+ * Return: 0
+ * */
+
+int main(void)
+{
+	char l_letter;
+
+	for (l_letter = 97; l_letter <= 122; l_letter++)
+	{
+		putchar(l_letetr);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
