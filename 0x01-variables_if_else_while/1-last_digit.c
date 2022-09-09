d71553d9f06d4cc82cebccd85fd78853af2abf6d
nzakawa <nzakawa1@gmail.com>
2022-09-09 11:03:26 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,6 @@ printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
 else if (last_digit == 0)
 printf("Last digit of %i is %i and is 0\n", n, last_digit);
 else if (last_digit < 6)
-printf("Last digit of %i is %i and is less than 6 and 0\n", n, last-digit);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 return (0);
 }
