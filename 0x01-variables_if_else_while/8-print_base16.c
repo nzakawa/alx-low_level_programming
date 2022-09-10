eb782828d6c27af7cf5591b146dffc2ed3d4e7b2
nzakawa <nzakawa1@gmail.com>
2022-09-10 03:47:23 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 /**
- * main -  prints all possible combinations of single-digit numbers
- * Return: 0
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line
+ * Return: Always 0.
  */
 int main(void)
 {
-int num;
-char letter;	
-for (num = 48; num <= 57; num++)
-{
-putchar(num);
-}
-for (letter = 'a'; letter <= 'f'; letter++)
-{
-putchar(letter);
-}
-putchar('\n');
+	int num;
+	char letter;
 
-return (0);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
 }
