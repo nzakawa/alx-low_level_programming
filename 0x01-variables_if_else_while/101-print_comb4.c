df10e17b9e8fa66ee95c47d8877694aa6dbc6059
nzakawa <nzakawa1@gmail.com>
2022-09-10 11:33:12 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 int num1, num2, num3;
-for (num1 48; num1 <= 57; num1++)
+for (num1 = 48; num1 <= 57; num1++)
 {
-for(num2 = num1 + 1; num2 <= 57; num2++)
+for (num2 = num1 + 1; num2 <= 57; num2++)
 {
-for(num3 = num2 + 1; num3 <= 57; num3++)
+for (num3 = num2 + 1; num3 <= 57; num3++)
 {
 putchar(num1);
 putchar(num2);
