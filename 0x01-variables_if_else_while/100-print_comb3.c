2a2872ff9ed7e355abd2302a9ad963c29cbdffe4
nzakawa <nzakawa1@gmail.com>
2022-09-10 05:05:12 -0700

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 /**
- * maint - Write a program that prints different combinations of two digits
+ * maint - prints different combinations of two digits
  * Return: 0
  */
-
 int main(void)
 {
 	int num1, num2;
 	for (num1 = 48; num1 <= 57; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 57; num2++)
-		{
-			putchar(num1);
-			putchar(num2);
-
-			if (!(num1 == 56 && num2 == 57))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (num2 = num1 + 1; num2 <= 57; num2++)
+	{
+		putchar(num1);
+		putchar(num2);
+	
+	if (!(num1 == 56 && num2 == 57))
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
