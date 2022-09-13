86dc35059a2c2372a7e30b57553c3dc427182dd2
nzakawa <nzakawa1@gmail.com>
2022-09-13 12:14:24 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * main - print alphabet in lowercase
- *
- * Return: 0 
+ * main - print 10 times lowercase alphabet
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
-int n, co;
-co = 0;
-while (com< 10)
+int y;
+int x;
+
+for (x = 0; X < 10; x++)
 {
-	for (n = 'a'; n <= 'z'; n++)
+	for (j = 'a'; j <= 'z'; y++)
 	{
-	_putchar(n);
+		_putchar(y);
 	}
-	co++;
-	_putchar('\n')
-}
+	_putchar('\n');
+	}
 }
