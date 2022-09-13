6880456fb8459de6efbadc056b5e7ffde2e0b906
nzakawa <nzakawa1@gmail.com>
2022-09-13 10:30:09 -0700

    _isloer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - prints 10 times the alphabet, in lowercase, followed by a new line
+ * 
+ * Return: Always 0.
+ */
+int _islower(int r)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
