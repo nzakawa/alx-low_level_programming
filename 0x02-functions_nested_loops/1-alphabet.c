accac23adbff33fb09b71573a981113aa6ca9eb2
nzakawa <nzakawa1@gmail.com>
2022-09-13 10:04:34 -0700

    hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,11 @@
  */
 void print_alphabet(void)
 {
-	int n;
-for (n = 'a'; n <= 'z'; n++) 
-	{
-	_putchar(n);
-	}
-	_putchar('\n');
+int n;
+for (n = 'a'; n <= 'z'; n++)
+
+{
+_putchar(n);
+}
+_putchar('\n');
 }
