a0ba7ccf4899f775a4e14ca32ac7240e408bb508
nzakawa <nzakawa1@gmail.com>
2022-09-13 11:45:07 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: 0 
+ */
+void print_alphabet_x10(void)
+{
+int n, co;
+co = 0;
+while (com< 10)
+{
+	for (n = 'a'; n <= 'z'; n++)
+	{
+	_putchar(n);
+	}
+	co++;
+	_putchar('\n')
+}
+}
