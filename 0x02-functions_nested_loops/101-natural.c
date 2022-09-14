69b694ee016a3d1eb85814fb4b6739b4430ee400
nzakawa <nzakawa1@gmail.com>
2022-09-14 10:28:48 -0700

    natural number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the sum of mulitiples number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; 1 < 1024; i++)
+		if (i % 3 == 0 && i % 5 == 0)
+			sum += i;
+	printf("%d\n", sum);
+	return (0);
+}
