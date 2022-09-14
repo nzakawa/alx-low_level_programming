1f1f34b68d4e75fc99cd41874bfd418121b84b5e
nzakawa <nzakawa1@gmail.com>
2022-09-14 10:59:58 -0700

    natural number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, sum;
 
-	for (i = 0; 1 < 1024; i++)
+	for (i = 0; i < 1024; i++)
 		if (i % 3 == 0 || i % 5 == 0)
-			sum += i;
+			sum  += i;
 	printf("%d\n", sum);
 	return (0);
 }
