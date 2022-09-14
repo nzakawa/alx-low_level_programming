1a05a82a5c446fe5ab2e69ad99d47f47117cc47a
nzakawa <nzakawa1@gmail.com>
2022-09-14 04:49:27 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,30 +2,31 @@
 /**
  * jack_bauer - function that print every minute of the day
  * h = hour, m = min
+ * reset m
  * Return: 0
  */
 void jack_bauer(void)
 {
 	int h = 0;
 	int m = 0;
-	int h_reminder;
-	int m_reminder;
+	int h_remainder;
+	int m_remainder;
 
 	while (h <= 23)
 	{
 	while (m <= 59)
 	{
-	m_reminder = m % 10;
-	h_reminder = h % 10;
+	m_remainder = m % 10;
+	h_remainder = h % 10;
 	_putchar(h / 10 + '0');
-	_putchar(h_reminder + '0');
+	_putchar(h_remainder + '0');
 	_putchar(':');
 	_putchar(h / 10 + '0');
-	_putchar(m_reminder + '0');
+	_putchar(m_remainder + '0');
 	m++;
 	_putchar('\n');
 	}
 	h++;
 	m = 0;
-	}
+}
 }
