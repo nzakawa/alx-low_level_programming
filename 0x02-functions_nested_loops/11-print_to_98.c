3065961e5b4629bf6380c617917bcb18713b66ec
nzakawa <nzakawa1@gmail.com>
2022-09-14 07:44:53 -0700

    fix error

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,37 +7,16 @@
  */
 void print_to_98(int n)
 {
-	if (n < 0)
+	if (n < 98)
 	{
-		for (; n < 0; ++n)
-		{
+		for (n = n; n < 98; n++)
 			printf("%d, ", n);
-		}
-		for (; n <= 98; ++n)
-		{
-		if (n < 98)
-			printf("%d, ", n);
-		else
-		printf("%d", n);
-		}
-	} else if (n > 98)
-
-	{
-		for (; n >= 98; --n)
-		{
-			if (n > 98)
-				printf("%d, ", n);
-		}
-	} 	else
-	{
-		for (; n <= 98; ++n)
-		{
-			if (n < 98)
-				printf("%d, ", n);
-			else
-			printf("%d", n);
-		}
+		printf("%d\n", 98);
 	}
-	printf("\n");
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d,", n);
+		printf("%d\n", 98 );
+	}
 }
-
