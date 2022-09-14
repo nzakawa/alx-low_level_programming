4694d2bdd968709b0deebe4e3365df2b3327f150
nzakawa <nzakawa1@gmail.com>
2022-09-14 11:16:13 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fibonacci - print 100 fibonacci numners
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long count, x, y, z;
+	x = 0;
+	y = 1;
+	for (count = 0; count < 50; count++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		printf("%lu", z);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
