6c3d90b65076ef86532e10475c23a5a11270af49
nzakawa <nzakawa1@gmail.com>
2022-09-14 08:51:59 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,10 @@
 void jack_bauer(void)
 {
 	int h, m;
-	for (h = 0; h < 24; h++)
+
+	for (h = 0; h <= 24; h++)
 	{
-		for (m = 0; m < 60; h++)
+		for (m = 0; m <= 60; h++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
