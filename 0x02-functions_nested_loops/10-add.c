959fb88b00d3711d1cea3238b3b898366431a563
nzakawa <nzakawa1@gmail.com>
2022-09-14 05:54:33 -0700

    add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers and return result
+ * @x: first number
+ * @y: second number
+ * Return: Always 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
