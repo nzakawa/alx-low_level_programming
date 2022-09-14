e061cfda729cedc40c9120343958eba59516a46e
nzakawa <nzakawa1@gmail.com>
2022-09-14 11:36:12 -0700

    fibonacci 2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - sum of fibobanacci up to 4 millions
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long count, x, y, z, sum;
+	x = sum = 0;
+	y = x;
+	for (count = 0; count < 50; count++)
+	{
+		z = x + j;
+		x = y;
+		y = z;
+		if (z % 2 == 0 && z < 4000000)
+		{
+			sum += z;
+		}
+	}
+	printf("%lu\n", sum)
+		return (0);
+}
+
