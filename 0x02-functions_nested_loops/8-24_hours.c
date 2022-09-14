f27befa446eca1464f452a09229b4a133a847a8e
nzakawa <nzakawa1@gmail.com>
2022-09-14 02:37:23 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,16 +9,34 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 24; h++)
+	for (h = 0; h < 24; h++)
 	{
-	for (m = 0; m <= 60; m++)
+	for (m = 0; m < 60; m++)
 	{
-		_putchar((h / 10) + '0');
-		_putchar((h % 10) + '0');
+		if (h < 10)
+		{
+			_putchar('0');
+			_putchar(h + '0')
+		}
+		else if (h >= 10)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+		}
+		if (m < 10)
+		{
 		_putchar(':');
-		_putchar((m / 10) + '0');
-		_putchar((m % 1) + '0');
-		_putchar('\n');
+		_putchar('0');
+		_putchar(m);
+		}
+		else if (m > 10)
+		{
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+		}
 	}
+	_putchar('\n');
 	}
 }
+
