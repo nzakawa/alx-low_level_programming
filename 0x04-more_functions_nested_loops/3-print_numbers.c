59e5676b7ef2ed176936c8f018501af7d6b03ede
nzakawa <nzakawa1@gmail.com>
2022-09-15 15:00:13 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - printing more numbers
+ * @n: a number
+ * Return: 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; x < 10; x++)
+		_putchar(x + '0')
+	_putchar('\n');
