2e40e11c4865f58a6afbf278b2d985c06dc56552
nzakawa <nzakawa1@gmail.com>
2022-09-15 15:17:13 -0700

    printing num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - fucntion for printing number 0 to 9
+ * print_numbers - printing numbers
  * Return: 0.
  */
 void print_numbers(void)
 {
-	int n;
+	int z;
 
-	for (n = 0; x < 10; x++)
-		_putchar(x + '0')
+	for (z = 0; z < 10; x++)
+		_putchar(z + '0')
 	_putchar('\n');
 }
