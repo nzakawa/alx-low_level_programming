e813d45e1381c14d9675dfe47dfd01653d397279
nzakawa <nzakawa1@gmail.com>
2022-09-16 08:01:53 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+whiel (n-- > 0)
+{
+putchar('_');
+}
+putchar('\n')
+}
