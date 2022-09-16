ee58ec2d3813de30f5fb69026b6cde5edede6c02
nzakawa <nzakawa1@gmail.com>
2022-09-16 13:29:07 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,18 @@
 #include "main.h"
 
 /**
- * print_line - prints a straight line
- * @n: number
+ * print_line - draw a straight line
+ * @n: number it lines
  * Return: 0
  */
-
 void print_line(int n)
 {
-	whiel(n-- > 0)
-{
-putchar('_');
-}
-putchar('\n')
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
 }
