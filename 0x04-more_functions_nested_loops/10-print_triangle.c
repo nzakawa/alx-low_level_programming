ad842933b78745c8a63417f5737fbe2ac033e5ff
nzakawa <nzakawa1@gmail.com>
2022-09-16 14:43:52 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: type of integer
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i = 1;
+	int m;
+
+	while (i <= size && size - 1)
+	{
+		m = 0;
+		while (m < size - 1)
+		{
+			_putchar(' ');
+			m++
+		}
+		m = 0;
+		while (m < 1)
+		{
+			_putchar('#');
+			m++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
+}
+
