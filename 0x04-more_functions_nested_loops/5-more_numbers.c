434effd9e38b1bc4a05fe6d64aa7e5035464e260
nzakawa <nzakawa1@gmail.com>
2022-09-16 13:10:43 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 void more_numbers(void)
 {
 	char n, c;
-	int i = 0
+	int i = 0;
 
 		while (i < 10)
 		{
 		for (n = 0; n <= 14; n++)
 		{
 		c = n;
-		if(n > 9)
+		if (n > 9)
 		{
 			_putchar('1');
 			c = n % 10;
