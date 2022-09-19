38fe0fe2be15e26062a7566875f543479c40a61a
nzakawa <nzakawa1@gmail.com>
2022-09-19 12:24:51 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - fumction that prints an array of numbers
+ * @x: integer array
+ * @n: integer number
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+int j;
+
+j = 0;
+for (n--; n >= 0; n--, j++)
+{
+printf("%d", a[j]);
+if (n > 0)
+{
+printf(",");
+}
+}
+printf("\n");
+}
+
