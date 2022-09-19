0016df05a6ea194f9e95f679c001b02bed3a979f
nzakawa <nzakawa1@gmail.com>
2022-09-19 09:50:55 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,15 @@
 
 void puts2(char *str)
 {
-int x, y;
+int x = 0;
+while (str[x] != '\0')
+{
+	if (x % 2 == 0)
+	{
+		_putchar(str[x]);
+	}
 
-x = strx(str);
-for (y = 0; y < x; y += 2)
-_putchar(str[x]);
+	x++;
+}
 _putchar('\n');
 }
