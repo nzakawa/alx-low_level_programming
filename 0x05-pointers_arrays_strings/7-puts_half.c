b58cccdd6fc18a25b537b110138e147621c590ae
nzakawa <nzakawa1@gmail.com>
2022-09-19 12:09:20 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,14 @@
  */
 void puts_half(char *str)
 {
-int a, b, c;
+int x;
 
-a = strlen(str);
-if (a % 2 == 1)
-b = a / 2 + 1;
-else
-b = a / 2;
-for (c = b; c < a;  c++)
-_putchar(str[c]);
+for (x = 0; str[x] != '\0'; x++)
+	;
+x++;
+for (x /= 2; str[x] != '\0'; x++)
+{
+_putchar(str[x]);
+}
 _putchar('\n');
 }
