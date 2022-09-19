a8145988f2954e48984da178fcbee9498b6a8983
nzakawa <nzakawa1@gmail.com>
2022-09-19 10:21:40 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - function thta prints second half of string
+ * @str: this is an aarray string type
+ * Return 0
+ */
+void puts_half(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	x++;
+	for (x /= 2; str[x] != '\0'; i++)
+	{
+		_putchar(str[]);
+	}
+	_putchar('\n');
+}
