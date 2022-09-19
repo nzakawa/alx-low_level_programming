ca015a24ee41ae6bb8d080a770975985d95639f3
nzakawa <nzakawa1@gmail.com>
2022-09-19 05:58:30 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * _puts - functions the prints string
+ * _puts - functions that prints string
  * @str: pointer to the string
  * Return: 0
  */
 void _puts(char *str)
 {
 	int j = 0;
-
 	while (str[j])
 	{
 		_putchar(str[j]);
-		J++;
-	}
+		j++;
+       	}
 	_putchar('\n');
 }
