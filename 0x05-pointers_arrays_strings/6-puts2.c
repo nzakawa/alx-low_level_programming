d78f6b3ebc65ac8a519869da3fb1898c969599df
nzakawa <nzakawa1@gmail.com>
2022-09-19 09:40:51 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - function that prints each string character
+ * @str: pointer to the string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+int x, y;
+x = strx(str);
+for (y = 0; y < x; y = y + 2)
+_putchar(str[x]);
+_putchar('\n');
+}
