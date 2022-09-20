d388b4861f014c4240125265e55e66d2bf0f7140
nzakawa <nzakawa1@gmail.com>
2022-09-20 14:41:37 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert string to integer
+ * @s: string type
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int j;
+	int k;
+	int sig = 1;
+	int rsp = 0;
+
+for (j = 0; !(s[j] >= 48 && s[j] <= 57); j++)
+	if (s[j] == '-')
+	{
+		sig = sig -1;
+	}
+for (k = j; s[k] >= 48 && s[k] <= 57; k++)
+{
+	rsp = rsp * 10;
+	rsp = rsp + (s[k] - 48);
+}
+	rsp = rsp * sig;
+	return (rsp);
+}
+
+
