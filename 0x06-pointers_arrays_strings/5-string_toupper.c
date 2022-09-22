792d0e4335357e32660ee8c6429ef2cd86418bef
nzakawa <nzakawa1@gmail.com>
2022-09-22 13:57:09 -0700

    string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - function that changes lowercase to uppercase string
+ * @s: string
+ * Return: 0
+ */
+char *string_toupper(char *s)
+{
+	char *pc = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s -= 32;
+		}
+		s++;
+	}
+	return (cp);
+}
