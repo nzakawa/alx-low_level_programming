7c853afe415e5220d3a3b5d9053a00002b928245
nzakawa <nzakawa1@gmail.com>
2022-09-22 13:12:06 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int j = 0;
+int j;
 
-	while ((s1[j] == s2[j] && (s1[j] != '\0'))
+for (j = 0; s1[j] != '\0'  && s1[j] != '\0'; j++)
+{
+	if (s1[j] != s2[j])
 	{
-	j++;
-	}
 	return (s1[j] - s2[j]);
+	}
+
 }
+	return (0);
+}
+
