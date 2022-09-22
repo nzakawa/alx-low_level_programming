d1adc2101e4bda592649dd131be96aa8bfe010d5
nzakawa <nzakawa1@gmail.com>
2022-09-22 11:30:10 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies strings
+ * @dest: destination pointer
+ * @src: search poiter
+ * @n: integer number
+ * Return: 0;
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int src_len = 0, j = 0;
+char *pc = dest, *c = src;
+while (*src)
+{
+src_len++;
+src++;
+}
+src_len++;
+if (n > src_len)
+{
+n = src_len;
+}
+src = c;
+for (; j < n; j++)
+{
+*dest++ = *src++;
+}
+return (pc);
+}
+
