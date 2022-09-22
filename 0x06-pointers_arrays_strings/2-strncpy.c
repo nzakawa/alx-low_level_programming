676dc0a2c940874bc523d55f93ef44840aba3450
nzakawa <nzakawa1@gmail.com>
2022-09-22 11:46:13 -0700

    hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,14 +7,19 @@
  * @n: integer number
  * Return: 0;
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 int src_len = 0, j = 0;
 char *pc = dest, *c = src;
+
 while (*src)
 {
 src_len++;
 src++;
+}
+while (*dest)
+{
+dest++;
 }
 src_len++;
 if (n > src_len)
