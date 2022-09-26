733e5b027538c91106edf3f3b6bb0e21d1f1fbfd
nzakawa <nzakawa1@gmail.com>
2022-09-26 15:30:20 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - function that locate character in a string
+ * @s: string
+ * @c: constant
+ * Return: 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	for (j = 0; s[j] >= '\0'; j++)
+	{
+		if (s[j] == c)
+			return (s + 1);
+	}
+	return ('\0');
+}
