635e11536c9b2b87ea0676f072547bda3dbfecdd
nzakawa <nzakawa1@gmail.com>
2022-09-26 13:04:41 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies memory area
+ * @dest: destionation pointer
+ * @src: source pointer
+ * @n: integer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; n > 0; j++, n--)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
