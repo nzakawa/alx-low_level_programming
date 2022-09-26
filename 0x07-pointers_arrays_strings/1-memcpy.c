aa086152f23f77bd98c22b6e94826beaeda20158
nzakawa <nzakawa1@gmail.com>
2022-09-26 15:09:53 -0700

    hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,9 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; n > j; j++)
 	{
-		src[j] = dest[j];
+		dest[j] = src[j];
 	}
 	return (dest);
 }
