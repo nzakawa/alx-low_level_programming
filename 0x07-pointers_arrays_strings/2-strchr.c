9f1f1d6425bfc526a9c50b51b9452b3f8558f43d
nzakawa <nzakawa1@gmail.com>
2022-09-26 15:37:11 -0700

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,13 @@ char *_strchr(char *s, char c)
 {
 	int j;
 
-	for (j = 0; s[j] >= '\0'; j++)
+	for (j = 0; s[j] >= '\0' ; j++)
 	{
 		if (s[j] == c)
+		{
 			return (s + 1);
+		}	
 	}
+
 	return ('\0');
 }
