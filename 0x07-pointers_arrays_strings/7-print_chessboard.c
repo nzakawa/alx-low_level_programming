bfe91e221e7ce73c82403f9259780ac56c7c426c
nzakawa <nzakawa1@gmail.com>
2022-09-26 21:38:39 -0700

    ask 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints a chessbord
+ * @a: The chessboard to be printed
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	if (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 0; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
