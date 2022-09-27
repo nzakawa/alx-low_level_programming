9994e633827a55a90cdf773c3c488b67b03e3233
nzakawa <nzakawa1@gmail.com>
2022-09-26 21:57:32 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+/*
+ * File: 8-print_diagsums.c
+ * Auto: pvincent
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of two diagonals
+ * @a: matrix of integer.
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int j, sum1 = 0, sum2 = 0;
+
+	for (j = 0; j < size; j++)
+	{
+		sum1 += a[j];
+		a += size;
+	}
+	a -= size;
+
+	for (j = 0; j < size; j++)
+	{
+		sum2 += a[j];
+		a -= size;
+	}
+	print("%d, %d\n", sum1, sum2);
+}
+
