924450bce8f57c2fcd37f069021413f920312648
nzakawa <nzakawa1@gmail.com>
2022-09-26 23:15:46 -0700

    hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_chessboard - function that prints a chessbord
  * @a: The chessboard to be printed
@@ -8,10 +8,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	if (i = 0; a[i][7]; i++)
+	for (i = 0; a[i][7]; i++)
 	{
 		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
+
 		_putchar('\n');
 	}
 }
