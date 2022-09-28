52bf05b36d7e7f162d53787ba53a81ed130927d6
nzakawa <nzakawa1@gmail.com>
2022-09-28 13:06:02 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,7 @@ int _strlen_recursion(char *s)
 {
 	int j = 0;
 
-	if (*s > '\0')
-	{
-		j += _strlen_recursion(s + 1) + 1;
-	}
-
-	return (n);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
