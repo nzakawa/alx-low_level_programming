2b6f2cf3449ec5c840a9b4b3f4b2a5aaa82c5a7c
nzakawa <nzakawa1@gmail.com>
2022-09-28 14:17:52 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * evaluate_number(int n, int m) - evaluate if the number is prime
+ *@n: integer
+ *@m: prime number
+ * Return: 1 if prime, 0 if not
+ */
+int evaluate_number(int n, int m)
+{
+	if (m < 2 || m % n == 0)
+		return (0);
+	else if (n > m / 2)
+		return (1);
+	else
+		return (evaluate_number(f + 1, m));
+}
+
+/**
+ * int is_prime_number - indicate if the number is prime
+ * @n: integer
+ *
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (evaluate_number(2, n));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square(int n, int m);
+int evaluate_number(int n, int m);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
