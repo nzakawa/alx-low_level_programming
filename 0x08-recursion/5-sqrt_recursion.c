c21d030234e68436bacffb4828cb58d1397dc7c5
nzakawa <nzakawa1@gmail.com>
2022-09-28 13:45:44 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,22 @@
 #include "main.h"
-
 /**
  * _sqrt_recursion - function that returns the natural square root of a n
  * @n: integer
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * _sqrt_recursion - function that finds square root of a n
+ * @n: integer
  * @m: square value
+ * Return: integer
  */
 
-int _sqrt_recursion(int n)
+int square(int n, int m)
 {
 	if (m * m == n)
 		return (m);
