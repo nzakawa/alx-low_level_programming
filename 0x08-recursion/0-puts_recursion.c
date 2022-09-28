fedc4158a24cd1236a394d777580265ddd7c520c
nzakawa <nzakawa1@gmail.com>
2022-09-28 12:24:21 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * _puts_recursion - function that prints a string, followed by a new line
  * @s: pointer to the block of memory
- * Return: 0.
+ * Return: void
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
