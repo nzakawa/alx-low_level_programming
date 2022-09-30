297bae7af770bb50dcc5c88bd5eb790bec5ea184
nzakawa <nzakawa1@gmail.com>
2022-09-30 13:33:18 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int ans, n1, n2;
-
-	if (argc != 3)
+	if (argc < 2)
 	{
-		printf("%s\n", "Error");
-		return (1);
+		printf("Error");
 	}
-	n1 = argv[1];
-	n2 = argv[2];
-	ans = n1 * n2;
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 
-	printf("%d\n", ans);
 	return (0);
 }
