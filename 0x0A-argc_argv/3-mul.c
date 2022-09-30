bae308eca0231526181631d3e1528e38ce7fb074
nzakawa <nzakawa1@gmail.com>
2022-09-30 07:44:30 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,12 @@
 
 int main(int argc, char const *argv[])
 {
-(void)argc;
 
-if (argc != 3)
-{
-	printf("Error\n");
-	return (1);
-}
-printf("%\n", atoi(argv[1]) * atoi(argv[2]));
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
