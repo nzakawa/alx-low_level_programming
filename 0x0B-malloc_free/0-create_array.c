a8c2cb2349b7d65d3687e6d462f71f9b22e324db
nzakawa <nzakawa1@gmail.com>
2022-10-03 12:39:54 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,11 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+	array = mallac(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
 	for (index = 0; index < size; index++)
 		array[index] = c;
 	return (array);
