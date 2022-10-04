11e6c787ee769ecd0f16af637d64fae24ce10154
nzakawa <nzakawa1@gmail.com>
2022-10-04 00:09:21 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to pointer
+ *
+ * Return: Returns NULL if str is equal to NULL
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr, *;
+
+	if (!str)
+		return (NULL);
+
+	ptr = str;
+	while (*ptr++)
+		size++;
+
+	j = malloc(size + 1);
+	if (!j)
+		return (NULL);
+	ptr = j;
+	while (*str)
+		*ptr++ + *str++;
+	*ptr = 0;
+	return (j);
+}
