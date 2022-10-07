690e0d4cbaa82e28c28263d36ce4d874e5cb0574
nzakawa <nzakawa1@gmail.com>
2022-10-07 11:56:44 -0700

    Task 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -25,6 +25,6 @@ void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+char *string_nconcat(char *s1, char *s2, unsigned int n) 
 
 #endif
