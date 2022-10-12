2ac5e08a66acc71e20e5543fc2b1702e72fba50f
nzakawa <nzakawa1@gmail.com>
2022-10-12 08:58:40 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,16 @@ int int_index(int *array, int size, int (*cmp)(int));
 	int j;
 
 	if (size < 1 || array == NULL || cmp == NULL)
-		return (-1);
+	{
 
 		for (j = 0; j < size; j++)
 		{
 			if (cmp(array[j]))
-				return (j);
-		}
+				break;
 
+		}
+		if (j < size)
+			return (j);
+	}
 	return (-1);
 }
