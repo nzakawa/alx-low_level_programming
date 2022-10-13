3a6fcea1e48d92aaa941861e155bbd873fc8986c
nzakawa <nzakawa1@gmail.com>
2022-10-13 15:32:36 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_c - prints a char
+ * @list: name va_list
+ *
+ * Return: void
+ */
+void print_c(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_i - print an integer
+ * @list: name va_list
+ *
+ * Return: void
+ */
+void print_i(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+
+/**
+ * print_f - print float numbers
+ * @list: name of Va_list
+ *
+ * Return: void
+ */
+
+void print_f(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_s - print string
+ * @list: name va_list
+ *
+ * Return: void
+ */
+
+void print_s(va_list list)
+{
+	char *ptr;
+
+	ptr = va_arg(list, char *);
+	if (ptr == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", ptr);
+}
+
+/**
+ * print_all - check the code for Holberton School students
+ * @format: number of arguments in character format
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	va_list list;
+	char *separator = "";
+
+	print_t p[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		};
+
+	va_list(list, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (p[j].t[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				p[j].f(list);
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(list);
+}
+
+
