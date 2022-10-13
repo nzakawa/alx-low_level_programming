a6505b62205b2c4f50aef14496583d845b153be2
nzakawa <nzakawa1@gmail.com>
2022-10-13 05:41:33 -0700

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,12 @@
-#ifndef VARIADIC_FUNCTIONSH
-#define VARIADIC_FUNCTIONSH
+#ifndef VARIADIC_FUNCTIONSH_H
+#define VARIADIC_FUNCTIONSH_H
+
 #include <stdarg.h>
 
 /**
  * struct print - print function
  * @p: print type
- * @: print function
+ * @f: print function
  */
 typedef struct print
 {
@@ -19,4 +20,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif /*VARIADIC_FUNCTIONSH*/
+#endif /*VARIADIC_FUNCTIONSH_H*/
