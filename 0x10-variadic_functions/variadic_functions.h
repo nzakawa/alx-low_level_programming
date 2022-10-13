f6c47a3a90466da44faf14d701537ecd48458766
nzakawa <nzakawa1@gmail.com>
2022-10-13 05:29:12 -0700

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,17 @@
 #define VARIADIC_FUNCTIONSH
 #include <stdarg.h>
 
+/**
+ * struct print - print function
+ * @p: print type
+ * @: print function
+ */
+typedef struct print
+{
+	char *p;
+	void (*f)(va_list);
+} print_p;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
