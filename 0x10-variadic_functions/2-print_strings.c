7254f012d948e4ad9583c2982d400f389db69c41
nzakawa <nzakawa1@gmail.com>
2022-10-13 10:58:48 -0700

    Task 1

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: item to use to separate the numbers
+ * @n: number of argument
+ *
+ * Return: sum of all argument
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int j;
+	char *ptr;
+	va_list list;
+
+	va_start(list, n);
+	for (j = 0; j < n; j++)
+	{
+		ptr = va_arg(list, char *);
+		if (ptr)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (j < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+}
