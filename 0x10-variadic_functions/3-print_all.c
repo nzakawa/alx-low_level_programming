7f2ed01c10aa6475260b277c5b67e73fc0dbaa45
nzakawa <nzakawa1@gmail.com>
2022-10-14 17:07:19 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,50 +4,50 @@
 
 /**
  * print_c - prints a char
- * @c: name va_list
+ * @form: name va_list
  *
  * Return: void
  */
-void print_c(va_list c)
+void print_c(va_list form)
 {
-	printf("%c", va_arg(list, int));
+	printf("%c", va_arg(form, int));
 }
 
 /**
  * print_i - print an integer
- * @list: name va_list
+ * @form: name va_list
  *
  * Return: void
  */
-void print_i(va_list list)
+void print_i(va_list form)
 {
-	printf("%i", va_arg(list, int));
+	printf("%i", va_arg(form, int));
 }
 
 /**
  * print_f - print float numbers
- * @list: name of Va_list
+ * @form: name of Va_list
  *
  * Return: void
  */
 
-void print_f(va_list list)
+void print_f(va_list form)
 {
-	printf("%f", va_arg(list, double));
+	printf("%f", va_arg(form, double));
 }
 
 /**
  * print_s - print string
- * @list: name va_list
+ * @form: name va_list
  *
  * Return: void
  */
 
-void print_s(va_list list)
+void print_s(va_list form)
 {
 	char *ptr;
 
-	ptr = va_arg(list, char *);
+	ptr = va_arg(form, char *);
 	if (ptr == NULL)
 	{
 		printf("(nil)");
@@ -65,38 +65,38 @@ void print_s(va_list list)
 void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
-	va_list all;
+	va_list list;
 	char *separator = "";
 
-	f p[] = {
+	print_t p[] = {
 		{"c", print_c},
 		{"i", print_i},
 		{"f", print_f},
 		{"s", print_s},
-		};
+		{NULL, NULL}
+	};
 
-	va_start(all, format);
+	va_start(list, format);
 	i = 0;
 	while (format && format[i])
 	{
 		j = 0;
-		while (j < 4)
+		while (p[j].t != NULL)
 		{
-			if (p[j].p[0] == format[i])
+			if (*(p[j].t) == format[i])
 			{
 				printf("%s", separator);
-				p[j].f(all);
+				p[j].f(list);
 				separator = ", ";
 				break;
 			}
 			j++;
 		}
-
-	i++;
+		i++;
 	}
 
 	printf("\n");
-	va_end(all);
+	va_end(list);
 }
 
 
