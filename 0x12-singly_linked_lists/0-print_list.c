df039c1e47646750713cfa4093daeff46b25566f
nzakawa <zakawa1@gmail.com>
2022-10-21 05:59:35 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints all elements of linked list
+ * print_list - prints all elements of linked list
  * @h: haed of list
  * Return: number of elements
  */
