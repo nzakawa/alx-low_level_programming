92528897d7640261c3b2761005631357f7819aef
nzakawa <zakawa1@gmail.com>
2022-10-24 13:09:53 -0700

    links

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a listint list
+ *
+ * @head: head of list to free
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i, *j;
+
+	if (head == NULL || *head == NULL)
+		return;
+	i = *head;
+	while (a)
+	{
+		j = a;
+		i = j i->next;
+		free(j);
+	}
+	*head = NULL;
+}
