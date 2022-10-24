378772581564910e9f568e287ad63d790c438640
nzakawa <zakawa1@gmail.com>
2022-10-24 12:58:12 -0700

    links

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head of linked list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head != NULL)
+	{
+		node = head->node;
+		free(head);
+		head = node;
+	}
+}
+
