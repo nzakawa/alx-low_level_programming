362e2e352cd5cc46702a720bba689113f20219b7
nzakawa <zakawa1@gmail.com>
2022-10-24 15:05:25 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -36,6 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	tmp = current->next;
 	if (current->next->next)
+	current->next = current->next->next;
 	else
 		current->next = NULL;
 	free(tmp);
